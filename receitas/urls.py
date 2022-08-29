b7ca85564f3be9582cc480cbddf0f707ed5adb2c
Lohan Pitta Verdan <lohanpitta1@gmail.com>
2022-08-29 17:45:49 -0300

    Seção 7

--- receitas/urls.py
+++ receitas/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 
 from . import views
 
+app_name = 'recipes'
+
 urlpatterns = [
-    path('', views.home),
-    path('receitas/<int:id>/', views.receitas),
+    path('', views.home, name="home"),
+    path('receitas/<int:id>/', views.receitas, name="recipe"),
 ]
